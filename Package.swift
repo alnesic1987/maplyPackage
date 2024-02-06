9f0652122740c498e88345b94bfcc0e8737fc75a
Aleksandar Nesic <al.nesic1987@gmail.com>
2024-02-06 14:57:13 +0100

    use binaryTarget directly

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MaplyPackage",
-            targets: ["MaplyPackage"]),
+            targets: ["WhirlyGlobeMaplyComponent"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -23,12 +23,12 @@ let package = Package(
 //            resources: [.process("Resources/Others"),
 //                        .copy("Resources/Maps")]
 //        ),
-        .target(
-            name: "MaplyPackage",
-            dependencies: [
-                "WhirlyGlobeMaplyComponent"
-            ]
-        ),
+//        .target(
+//            name: "MaplyPackage",
+//            dependencies: [
+//                "WhirlyGlobeMaplyComponent"
+//            ]
+//        ),
         .binaryTarget(
             name: "WhirlyGlobeMaplyComponent",
             path: "Sources/WhirlyGlobeMaplyComponent.xcframework"
