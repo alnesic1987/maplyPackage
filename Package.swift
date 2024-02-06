d21a75e8fe931cec03f7191aa4753a54a421f045
Aleksandar Nesic <al.nesic1987@gmail.com>
2024-02-06 10:38:11 +0100

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -14,10 +14,24 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+//        .target(
+//            name: "AppBuilder",
+//            dependencies: [
+//                .product(name: "EwasAuthentication", package: "ios-authentication"),
+//                "WhirlyGlobeMaplyComponent"
+//            ],
+//            resources: [.process("Resources/Others"),
+//                        .copy("Resources/Maps")]
+//        ),
         .target(
-            name: "MaplyPackage"),
-        .testTarget(
-            name: "MaplyPackageTests",
-            dependencies: ["MaplyPackage"]),
+            name: "MaplyPackage",
+            dependencies: [
+                "WhirlyGlobeMaplyComponent"
+            ]
+        ),
+        .binaryTarget(
+            name: "WhirlyGlobeMaplyComponent",
+            path: "Sources/WhirlyGlobeMaplyComponent.xcframework"
+        )
     ]
 )
